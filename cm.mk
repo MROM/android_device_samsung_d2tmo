fc51051a66a0b260b25e47a5f167bbcba93a0b00
mmontuori <michael.montuori@gmail.com>
2013-07-31 22:07:49 -0400

    fixed typo

--- cm.mk
+++ cm.mk
@@ -9,7 +9,6 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-<<<<<<< HEAD
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2tmo \
     BUILD_DISPLAY_ID=MROM \
